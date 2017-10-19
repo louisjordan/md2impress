fe3bf42325377c6d93a364a369f867de621c0186
Louis Jordan <louisjordan@hotmail.com>
2017-10-19 23:09:01 +0100

    Added test data check

--- test/mdtoimpress.test.js
+++ test/mdtoimpress.test.js
@@ -1,7 +1,21 @@
+const helpers = require('./helpers');
 const mdtoimpress = require('../src');
 
 describe('mdtoimpress', () => {
   test('should return a string', () => {
     expect(typeof mdtoimpress('hi')).toBe('string');
+  });
+
+  describe('should return the correct html output for a given markdown input', () => {
+    let inputs, outputs;
+
+    beforeAll(() => {
+      inputs = helpers.loadTestInputs();
+      outputs = helpers.loadTestOutputs();
+    });
+
+    test('test_1_input.md should return test_1_output.html', () => {
+      expect(mdtoimpress(inputs[0])).toBe(outputs[0]);
+    });
   });
 });
