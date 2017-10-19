6a86043b86da47bbe31ca6ff13be3388dd21823b
Louis Jordan <louisjordan@hotmail.com>
2017-10-19 23:08:47 +0100

    Added test data and test helpers

---
+++ test/data/test_1_input.md
@@ -0,0 +1,2 @@
+# Test 1 Slideshow
+## Test H2
---
+++ test/helpers.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const TEST_DATA_ROOT = './test/data';
+
+const loadTestInputs = () => {
+  return fs.readdirSync(TEST_DATA_ROOT).reduce((acc, file) => {
+    if (file.indexOf('input') > -1)
+      acc.push(fs.readFileSync(`${TEST_DATA_ROOT}/${file}`, 'utf8'));
+    return acc;
+  }, []);
+};
+
+const loadTestOutputs = () => {
+  return fs.readdirSync(TEST_DATA_ROOT).reduce((acc, file) => {
+    if (file.indexOf('output') > -1)
+      acc.push(fs.readFileSync(`${TEST_DATA_ROOT}/${file}`, 'utf8'));
+    return acc;
+  }, []);
+};
+
+module.exports.loadTestInputs = loadTestInputs;
+module.exports.loadTestOutputs = loadTestOutputs;
