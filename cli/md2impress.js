b3a4ec5bba4307742d9b688265521192e7cc726d
Louis Jordan <louisjordan@hotmail.com>
2018-02-16 19:03:56 +0000

    CLI tweaks

--- cli/md2impress.js
+++ cli/md2impress.js
@@ -1,15 +1,5 @@
 #!/usr/bin/env node
 
-/**
- * Process commandline parameters
- *
- * Steps:
- * - Use commander to process CLI params     (https://www.npmjs.com/package/commander)
- * - Read input file                          (https://nodejs.org/api/fs.html)
- * - Generate impress HTML
- * - Save impress HTML to output location
- */
-
 const program = require('commander');
 const fs = require('fs');
 const path = require('path');
@@ -19,27 +9,22 @@ const md2impress = require('../src');
 const { version } = require('../package');
 
 program
-  .version(version)
-  .usage('-i input.md -o output.html [options]')
-  .option('-i, --input <file|dir>', 'Markdown input file path')
-  .option('-o, --output <dir>', 'HTML output directory')
-  .option('-l, --layout [layout]', 'Presentation layout')
-  .option('-s, --style [style]', 'Presentation style')
-  .option('-t, --title [title]', 'Presentation title')
+  .version(version, '-v, --version')
+  .usage('md2impress --input <file|dir> --output <dir> [options]')
+  .option('-i, --input [input]', 'Markdown input directory or file path (default: current directory)')
+  .option('-o, --output [output]', 'HTML output directory (default: current directory)')
+  .option('-l, --layout [layout]', "Presentation layout (default: 'manual')")
+  .option('-s, --style [style]', "Presentation style (default: 'basic')")
+  .option('-t, --title [title]', 'Presentation title (default: input filename)')
   .parse(process.argv);
-
-if (!program.input) {
-  console.log('Error: An input path must be provided! *');
-  program.help();
-  process.exit();
-}
 
 /* Read input path -> generate html with md2impress -> write to output path */
 const basePath = process.cwd();
-const inputPath = path.resolve(basePath, program.input);
-const outputPath = program.output ? path.resolve(basePath, program.output) : helpers.getLocationFromPath(inputPath);
+const inputPath = program.input ? path.resolve(basePath, program.input) : basePath; // default input is cwd
+const outputPath = program.output ? path.resolve(basePath, program.output) : helpers.getLocationFromPath(inputPath); // default output is input directory
 const layout = program.layout;
 const style = program.style;
+const title = typeof program.title === 'string' ? program.title : '';
 
 const outputIsDir = fs.lstatSync(outputPath).isDirectory();
 const inputIsDir = fs.lstatSync(inputPath).isDirectory();
@@ -65,8 +50,8 @@ inputFilePaths.forEach((inPath, index) => {
   const filename = helpers.getFilenameFromPath(inPath);
   const outPath = path.resolve(outputPath, filename + '.html');
 
-  const title =
-    program.title && inputIsDir ? `${program.title}_${index}` : helpers.formatString(program.title || filename);
+  const docTitle =
+    title && inputIsDir && inputFilePaths.length > 1 ? `${title} ${index}` : title || helpers.formatString(filename);
 
   // read input file
   fs.readFile(inPath, 'utf8', (err, input) => {
@@ -74,12 +59,12 @@ inputFilePaths.forEach((inPath, index) => {
 
     try {
       // generate html
-      const html = md2impress(input, { layout, style, title });
+      const html = md2impress(input, { layout, style, title: docTitle });
 
       // write to output path
       fs.writeFile(outPath, html, err => {
         if (err) console.error(err);
-        console.log(`\n[\u2713] [${title}]`);
+        console.log(`\n[\u2713] [${docTitle}]`);
         console.log(`    [MD] ${inPath}\n  [HTML] ${outPath}`);
       });
     } catch (error) {
